715b01f6b32d5cc39b6f1e98d77a035278d92b14
Eigo ITO <itoh@ccg.ac.jp>
2023-12-05 09:44:12 +0900

    initial commit

---
+++ src/main/java/jp/ac/ccmc/crud_sample/model/Department.java
@@ -0,0 +1,33 @@
+package jp.ac.ccmc.crud_sample.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student> students;
+
+}
---
+++ src/main/java/jp/ac/ccmc/crud_sample/model/Student.java
@@ -0,0 +1,35 @@
+package jp.ac.ccmc.crud_sample.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @NotNull
+    @Min(0)
+    private Integer age;
+
+    @ManyToOne
+    private Department department;
+}
